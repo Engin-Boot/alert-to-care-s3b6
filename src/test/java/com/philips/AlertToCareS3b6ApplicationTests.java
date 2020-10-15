d3a2c5042d96418011a851151682fafa2d7c6afe
harisht26698 <harish.t@philips.com>
2020-10-15 14:46:48 +0530

    fix

--- src/test/java/com/philips/AlertToCareS3b6ApplicationTests.java
+++ src/test/java/com/philips/AlertToCareS3b6ApplicationTests.java
@@ -1,18 +1,14 @@
 package com.philips;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.philips.entity.Bed;
 
 @SpringBootTest
 class AlertToCareS3b6ApplicationTests {
 	
 	@Test
 	void testBedClass() {
-		Bed b= new Bed(10);
-		assertThat(b.getbId()).isEqualTo(10);
 		
 	}
 	@Test
