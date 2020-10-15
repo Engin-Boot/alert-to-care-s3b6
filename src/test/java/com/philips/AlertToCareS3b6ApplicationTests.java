1bf1e6835796c2aab4a70569653450ccfac40369
harisht26698 <harish.t@philips.com>
2020-10-15 14:35:09 +0530

    unit test corrected

--- src/test/java/com/philips/AlertToCareS3b6ApplicationTests.java
+++ src/test/java/com/philips/AlertToCareS3b6ApplicationTests.java
@@ -2,23 +2,18 @@ package com.philips;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.philips.dto.Bed;
+import com.philips.entity.Bed;
 
 @SpringBootTest
 class AlertToCareS3b6ApplicationTests {
 	
 	@Test
 	void testBedClass() {
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
-	    Date date = new Date(); 
-		Bed b= new Bed(10,100,formatter.format(date));
+		Bed b= new Bed(10);
 		assertThat(b.getbId()).isEqualTo(10);
-		assertThat(b.getPatientId()).isEqualTo(100);
+		
 	}
 	@Test
 	void testGetUri() {
