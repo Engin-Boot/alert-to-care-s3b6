14cc35b2ec503835902c543ca99e9016f4a5def3
Nadimpalli <vinay.n.varma189@gmail.com>
2020-10-30 14:34:13 +0530

    else condition added

--- AlertToCareGUI/GUI/src/app/setup/setup.component.ts
+++ AlertToCareGUI/GUI/src/app/setup/setup.component.ts
@@ -19,17 +19,15 @@ export class SetupComponent implements OnInit {
   ngOnInit(): void {
   }
     public registerType(shape:string,n:number){
-      console.log(shape);
-       
-      if(shape=="Lshape"){
+        if(shape=="Lshape"){
 
-        let resp=this.patientService.setupLshape(n);
-        resp.subscribe((data)=>this.message=data);
-      }
-      if(shape="ICU"){
-        let resp=this.patientService.setupICU(n);
-          resp.subscribe((data)=>this.message=data);
-      }
+            let resp=this.patientService.setupLshape(n);
+            resp.subscribe((data)=>this.message=data);
+        }
+        else if(shape="ICU"){
+            let resp=this.patientService.setupICU(n);
+            resp.subscribe((data)=>this.message=data);
+        }
     }
   // public addICU(shape:string,n:number){  
   //   let resp=this.patientService.setupICU(shape,n);
